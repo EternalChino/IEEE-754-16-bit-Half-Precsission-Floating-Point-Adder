2e7431a77c6a4faa3655d8644f69df94c6d1a11a
Angel Guadalupe Montalvo <amgelm10@gmail.com>
2025-12-11 14:55:28 -0600

    Delete Code/ClockLadder.sv

--- Code/ClockLadder.sv
+++
@@ -1,9 +0,0 @@
-module ClockLadder #(parameter N = 32)
-(
-input clock, clear,
-output logic [N-1:0] Y
-);
-always_ff @ (posedge clock, negedge clear)
-if (~clear) Y <= 1'b0;
-else Y <= Y + 1'b1;
-endmodule
